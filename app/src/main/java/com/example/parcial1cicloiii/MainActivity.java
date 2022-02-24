661095b8902163e68c2383c0d9a901b1baad5eaf
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2022-02-24 10:50:06 -0600

    Primer Commit

--- app/src/main/java/com/example/parcial1cicloiii/MainActivity.java
+++ app/src/main/java/com/example/parcial1cicloiii/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.parcial1cicloiii;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.parcial1cicloiii.Controlador.PagerController;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tab1,tab2,tab3,tab4;
+
+    PagerController pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewuno);
+        tab1 = findViewById(R.id.tabdesayuno);
+        tab2 = findViewById(R.id.tabAlmuerzo);
+        tab3 = findViewById(R.id.tabCena);
+        tab4 = findViewById(R.id.tabHome);
+
+        pagerAdapter = new PagerController(getSupportFragmentManager(),tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Obtenemos la posición del tab a mostrar
+                viewPager.setCurrentItem(tab.getPosition());
+                // Determinar cual fragment se presenta
+                if(tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if(tab.getPosition()==1){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if(tab.getPosition()==2){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if(tab.getPosition()==3){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
     }
+
+
 }
--- app/src/main/java/com/example/parcial1cicloiii/MenuPrincipal.java
+++ app/src/main/java/com/example/parcial1cicloiii/MenuPrincipal.java
@@ -1,14 +1,67 @@
 package com.example.parcial1cicloiii;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.parcial1cicloiii.Controlador.PagerController;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MenuPrincipal extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tab1,tab2,tab3,tab4,tab5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        tabLayout = findViewById(R.id.menu);
+        tab1 = findViewById(R.id.tab1);
+        tab2 = findViewById(R.id.tab2);
+        tab3 = findViewById(R.id.tab3);
+        tab4 = findViewById(R.id.tab4);
+        tab5 = findViewById(R.id.tab5);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Obtenemos la posición del tab a mostra
+                // Determinar cual fragment se presenta
+                if(tab.getPosition()==0){
+
+                    Intent siguiente = new Intent(MenuPrincipal.this,MainActivity.class);
+                    startActivity(siguiente);
+                }
+                if(tab.getPosition()==1){
+                    Intent siguiente = new Intent(MenuPrincipal.this,MainActivity.class);
+                    startActivity(siguiente);
+                }
+                if(tab.getPosition()==2){
+                    Intent siguiente = new Intent(MenuPrincipal.this,MainActivity.class);
+                    startActivity(siguiente);
+                }
+                if(tab.getPosition()==3){
+                    Intent siguiente = new Intent(MenuPrincipal.this,MainActivity.class);
+                    startActivity(siguiente);
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
