72dc6b51e89f7265af0a624a39f931616aa73f92
JoseLobos <88002545+JoseLobos@users.noreply.github.com>
2022-02-26 15:08:04 -0600

    reproductor de audio

--- app/src/main/java/com/example/parcial1cicloiii/ReproductorDeAudio.java
+++ app/src/main/java/com/example/parcial1cicloiii/ReproductorDeAudio.java
@@ -4,45 +4,141 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ReproductorDeAudio extends AppCompatActivity implements
-        View.OnClickListener  {
+public class ReproductorDeAudio extends AppCompatActivity {
 
-    Button Iniciar, Parar;
-    MediaPlayer mediaPlayer;
+    Button play_pause, btn_repetir;
+    MediaPlayer mp;
+    ImageView iv;
+
+    // para determinar cuál canción sigue
+    int repetir = 2, posicion = 0;
+
+
+    MediaPlayer vectormp [] = new MediaPlayer[3];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reproductor_de_audio);
 
-        Iniciar = (Button)findViewById(R.id.Iniciar);
-        Parar   = (Button)findViewById(R.id.Parar);
-        mediaPlayer = MediaPlayer.create(this,R.raw.sound);
-
-        Iniciar.setOnClickListener(this);
-        Parar.setOnClickListener(this);
+        play_pause = (Button)findViewById(R.id.btn_play);
+        btn_repetir = (Button)findViewById(R.id.btn_repetir);
+        iv = (ImageView)findViewById(R.id.imageView);
+        vectormp[0] = MediaPlayer.create(this,R.raw.race);
+        vectormp[1] = MediaPlayer.create(this,R.raw.sound);
+        vectormp[2] = MediaPlayer.create(this,R.raw.tea);
     }
-
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.Iniciar:
-                play();
-                break;
-            case R.id.Parar:
-                stop();
-                break;
+    public void PLayPause(View view){
+        if(vectormp[posicion].isPlaying()){
+            vectormp[posicion].pause();
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this,"Pausa", Toast.LENGTH_SHORT).show();
+        }else {
+            vectormp[posicion].start();
+            play_pause.setBackgroundResource(R.drawable.pausa);
+            Toast.makeText(this,"Play",Toast.LENGTH_SHORT).show();
         }
     }
-    private void play(){
-        mediaPlayer.start();
+    public void Stop(View view){
+        if(vectormp[posicion] != null){
+            vectormp[posicion].stop();
+
+            vectormp[0] = MediaPlayer.create(this,R.raw.race);
+            vectormp[1] = MediaPlayer.create(this,R.raw.sound);
+            vectormp[2] = MediaPlayer.create(this,R.raw.tea);
+            posicion = 0;
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            iv.setImageResource(R.drawable.portada1);
+            Toast.makeText(this,"Stop",Toast.LENGTH_SHORT).show();
+        }
     }
-    private void stop(){
-        mediaPlayer.stop();
+    public void Repetir(View view){
+        if(repetir == 1) {
+            btn_repetir.setBackgroundResource(R.drawable.no_repetir);
+            Toast.makeText(this, "No repetir",Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping((false));
+            repetir = 2;
+        }
+        else{
+            btn_repetir.setBackgroundResource(R.drawable.repetir);
+            Toast.makeText(this, "Repetir",Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping((true));
+            repetir = 1;
+        }
+    }
+    public void Siguiente(View view){
+        if(posicion < vectormp.length -1){
+            if(vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+                posicion++;
+                vectormp[posicion].start();
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.portada1);
+                }
+                else if (posicion == 1){
+                    iv.setImageResource(R.drawable.portada2);
+                }
+                else if (posicion == 2){
+                    iv.setImageResource(R.drawable.portada3);
+                }
+            }
+            else{
+                posicion++;
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.portada1);
+                }else if (posicion == 1){
+                    iv.setImageResource(R.drawable.portada2);
+                }else if (posicion == 2){
+                    iv.setImageResource(R.drawable.portada3);
+                }
+            }
+        }
+        else {
+            Toast.makeText(this,"No hay más canciones",Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void Anterior(View view){
+
+        if(posicion >= 1) {
+
+            if(vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+                vectormp[0] = MediaPlayer.create(this,R.raw.race);
+                vectormp[1] = MediaPlayer.create(this,R.raw.sound);
+                vectormp[2] = MediaPlayer.create(this,R.raw.tea);
+                posicion--;
+
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.portada1);
+                }else if (posicion == 1){
+                    iv.setImageResource(R.drawable.portada2);
+                }else if (posicion == 2){
+                    iv.setImageResource(R.drawable.portada3);
+                }
+
+                vectormp[posicion].start();
+
+            }else {
+                posicion--;
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.portada1);
+                }else if (posicion == 1){
+                    iv.setImageResource(R.drawable.portada2);
+                }else if (posicion == 2){
+                    iv.setImageResource(R.drawable.portada3);
+                }
+            }
+        }
+        else{
+            Toast.makeText(this,"No hay más canciones",Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
 
